b3574a4ab82ece83d7f130f22a42fb10c971edff
NNeast <zawislaktomasz4@gmail.com>
2018-06-03 17:57:25 +0200

    fixed import path

--- server/cmd/run.go
+++ server/cmd/run.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	_ "github.com/nneast/talkaneast/server/pkg/channels"
-	"github.com/nneast/talkaneast/server/pkg/core"
-	_ "github.com/nneast/talkaneast/server/pkg/messages"
-	_ "github.com/nneast/talkaneast/server/pkg/users"
+	_ "github.com/NNeast/talkaneast/server/pkg/channels"
+	"github.com/NNeast/talkaneast/server/pkg/core"
+	_ "github.com/NNeast/talkaneast/server/pkg/messages"
+	_ "github.com/NNeast/talkaneast/server/pkg/users"
 )
 
 func main() {
