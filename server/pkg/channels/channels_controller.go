a3d36e6bdf8a823c97fbe6c96afcac1f851e7041
NNeast <zawislaktomasz4@gmail.com>
2018-06-03 22:22:14 +0200

    added CreateChannel and ListChannels functions

--- server/pkg/channels/channels_controller.go
+++ server/pkg/channels/channels_controller.go
@@ -1,7 +1,12 @@
 package channels
 
 import (
+	"log"
+
 	"github.com/NNeast/talkaneast/server/pkg/core"
+	"github.com/fatih/structs"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type ChannelsController struct {
@@ -10,29 +15,44 @@ type ChannelsController struct {
 
 func (cc *ChannelsController) Register(app *core.Application) {
 	cc.app = app
-}
 
-/*
-func (cc *ChannelsController) EventSetChannel(user *users.User, data map[string]interface{}) {
-	if containsChannels(cc.app.cm.Channels, data["channel"].(string)) {
-		user.CurrentChannel.Name = data["channel"].(string)
-	} else {
-		cc.app.cm.sendNotification(user, "this channel does not exist")
+	indexErr := app.Db.C("channels").EnsureIndex(mgo.Index{
+		Key:    []string{"name"},
+		Unique: true,
+	})
+
+	if indexErr != nil {
+		log.Panicf("Failed to create unique username index in channels controller: %v", indexErr)
 	}
 }
 
-func (cc *ChannelsController) EventCreateChannel(user *users.User, data map[string]interface{}) {
-	log.Printf("event create channel")
-	if containsChannels(cc.app.cm.Channels, data["name"].(string)) {
-		cc.server.sendNotification(user, "this channel already exist")
+func (cc *ChannelsController) EventCreateChannel(client *core.Client, session *core.Session, data map[string]interface{}) {
+	ch := &Channel{
+		Name: data["name"].(string),
 	}
-	cc.server.Channels = append(cc.app.cm.Channels, &Channel{Name: data["name"].(string)})
+
+	ch.ID = bson.NewObjectId()
+
+	insertErr := cc.app.Db.C("channels").Insert(&ch)
+
+	if insertErr == nil {
+		event := core.CreateEvent("ChannelCreated", structs.Map(&ch))
+		client.SendEvent(&event)
+		log.Printf("event create channel")
+	}
 }
 
-func (cc *ChannelsController) EventListChannels(user *users.User, data map[string]interface{}) {
-	cc.app.sendChannelsToUser(user)
+func (cc *ChannelsController) EventListChannels(client *core.Client, session *core.Session, data map[string]interface{}) {
+	var results []Channel
+	err := cc.app.Db.C("channels").Find(nil).All(&results)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Printf("event list channels")
+	event := core.CreateEvent("ListChannels", results)
+	client.SendEvent(&event)
 }
-*/
+
 func init() {
 	core.ControllersToRegister.PushBack(&ChannelsController{})
 }
